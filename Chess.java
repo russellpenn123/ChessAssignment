d8898f981c365165a142288f334a2bec23a6a8b4
russellpenn123 <russellpenn123@gmail.com>
2018-03-20 15:55:16 +0000

    Added empty classes for chess pieces

---
+++ Chess.java
@@ -0,0 +1,22 @@
+// ===================== Initialises =====================
+
+// Constucts a new Board
+// A collection of Pieces
+// 2 Players
+
+// ===================== What it should do =====================
+
+// Request a move from each player in turn, which should be supplied as to and from co-ordinates
+// Checks whether move is valid, if it is then it will do the move
+// If not then the user is asked to repeat the input
+// Display the updated state of the game
+// If the king has been taken then the winner is declared
+
+
+public class Chess{
+  public static void main(String[] args){
+
+
+
+  }
+}
